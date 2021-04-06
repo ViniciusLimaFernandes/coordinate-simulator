00d12b8c844a0d6273bd86ebd758778cc744696d
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2021-04-06 01:34:47 -0300

    app structure and logics

---
+++ application/produce.go
@@ -0,0 +1,31 @@
+package application
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+
+	"github.com/ViniciusLimaFernandes/coordinate-simulator/application/route"
+	"github.com/ViniciusLimaFernandes/coordinate-simulator/infra/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func Produce(msg *ckafka.Message) {
+	producer := kafka.NewKafkaProducer()
+	route := route.NewRoute()
+
+	json.Unmarshal(msg.Value, &route)
+
+	route.LoadPositions()
+
+	positions, err := route.ExportJsonPositions()
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	for _, p := range positions {
+		kafka.Publish(p, os.Getenv("KafkaProduceTopic"), producer)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
