1b39a764cb2c7e84ed264d5be0161c00bba25bfa
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2021-04-06 01:32:33 -0300

    main with kafka producer and consumer config

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ViniciusLimaFernandes/coordinate-simulator/application"
+	"github.com/ViniciusLimaFernandes/coordinate-simulator/infra/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
+
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
+		go application.Produce(msg)
+	}
+}
